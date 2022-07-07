3d44e617d7c88fd1a3df8630f3b7de7b0a70b091
MortISense <motrinxd@gmail.com>
2022-07-07 10:29:26 -0700

    Pokedex Project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,87 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import "./App.css";
+import axios from "axios";
 
-function App() {
+const App = () => {
+  // sets the state for the pokemon data
+  const [pokemon, setPokemon] = useState("pikachu");
+  const [pokemonData, setPokemonData] = useState([]);
+  const [pokemonType, setPokemonType] = useState("");
+
+  const getPokemon = async () => {
+    const toArray = [];
+    try {
+      // URL for the API
+      const url = `https://pokeapi.co/api/v2/pokemon/${pokemon}`;
+      // Response awaits axios to grab from the url
+      const res = await axios.get(url);
+      //Pushs the data into the array
+      toArray.push(res.data);
+      //sets the pokemon type by grabbing from the array and that index
+      setPokemonType(res.data.types[0].type.name);
+      //sets the pokemon data using the array
+      setPokemonData(toArray);
+      console.log(res);
+    } catch (e) {
+      console.log(e);
+    }
+  };
+
+  const handleChange = (e) => {
+    setPokemon(e.target.value.toLowerCase());
+  };
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    getPokemon();
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form onSubmit={handleSubmit}>
+        <label>
+          <input
+            type="text"
+            onChange={handleChange}
+            placeholder="Enter Pokemon Name"
+          />
+        </label>
+      </form>
+      {/* //Loops over the pokemon data */}
+      {pokemonData.map((data) => {
+        return (
+          <div className="container">
+            <img src={data.sprites["front_default"]} />
+            <div className="divTable">
+              <div className="divTableBody">
+                <div className="divTableRow">
+                  <div className="divTableCell">Type</div>
+                  <div className="divTableCell">{pokemonType}</div>
+                </div>
+                <div className="divTableRow">
+                  <div className="divTableCell">Height</div>
+                  <div className="divTableCell">
+                    {" "}
+                    {Math.round(data.height * 3.9)}
+                  </div>
+                </div>
+                <div className="divTableRow">
+                  <div className="divTableCell">Weight</div>
+                  <div className="divTableCell">
+                    {" "}
+                    {Math.round(data.height / 4.3)} lbs
+                  </div>
+                </div>
+                <div className="divTableRow">
+                  <div className="divTableCell">Number of Battles</div>
+                  <div className="divTableCell">{data.game_indices.length}</div>
+                </div>
+              </div>
+            </div>
+          </div>
+        );
+      })}
     </div>
   );
-}
+};
 
 export default App;
